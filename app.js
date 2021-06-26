58a569c95758ea436d91f9ec9e676fa703bbf6bb
AlfieriChou <alfierichou@gmail.com>
2021-06-26 16:53:05 +0800

    feat: static face detection

--- app.js
+++ app.js
@@ -2,36 +2,15 @@ const Koa = require('koa')
 const koaBody = require('koa-body')
 const bodyParser = require('koa-bodyparser')
 const koaLogger = require('koa-logger')
-
-const FaceDetect = require('./lib/detect')
+const serve = require('koa-static')
+const path = require('path')
 
 const app = new Koa()
-const faceDetect = new FaceDetect()
 
 app.use(koaLogger())
 app.use(koaBody({}))
 app.use(bodyParser())
-app.use(async (ctx, next) => {
-  try {
-    if (ctx.request.path === '/run' && ctx.request.method === 'POST') {
-      const { url } = ctx.request.body
-      const ret = await faceDetect.run(url)
-      ctx.body = ret
-      await next()
-    } else {
-      ctx.body = {
-        code: 0,
-        message: ''
-      }
-    }
-  } catch (err) {
-    ctx.body = {
-      code: 400,
-      message: err.message,
-      stack: err.stack
-    }
-  }
-})
+app.use(serve(path.resolve(__dirname, './public')))
 
 app.listen(4000, () => {
   console.info('listening on 4000')
